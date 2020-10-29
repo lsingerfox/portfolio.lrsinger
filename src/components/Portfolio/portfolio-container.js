2558f697b9e4463ed16f30b3801c5450d49b0a4f
lynellsinger <lynell.r.singer@gmail.com>
2020-10-28 21:46:34 -0700

    Updated portfolio-container and portfolio-item

--- src/components/Portfolio/portfolio-container.js
+++ src/components/Portfolio/portfolio-container.js
@@ -3,12 +3,32 @@ import React, { Component } from "react";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            data: [
+                { title: "Deviantart" },
+                { title: "Linkedin" },
+                { title: "Smule" },
+                { title: "YouTube" }
+            ]
+        }
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        })
+    }
+
     render () {
         return (
             <div>
-                <h2>Portfolio items go here updated...</h2>
+                <h2>{this.state.pageTitle}</h2>
 
-                <PortfolioItem />
+                {this.portfolioItems()}
             </div>
         )
     }
