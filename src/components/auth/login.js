28d010be80d01cf29e46abc3bce3e304bd551d1b
lynellsinger <lynell.r.singer@gmail.com>
2020-11-15 13:38:43 -0800

    Updated login.js auth code

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,16 +1,54 @@
 import React, { Component } from "react";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    handleSubmit(event) {
+        console.log("Handle submit", event);
+    }
+
     render() {
         return (
             <div>
                <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
                
-               <form>
-                   <input type="text"/>
-                   <input type="password"/>
+               <form onSubmit={this.handleSubmit}>
+                   <input 
+                        type="email"
+                        name="email"
+                        placeholder="Your Email"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        />
+                    
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder="Your Password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                        />
                </form>
-               <div>Submit Button</div> 
+               
+               <div>
+                   <button type="submit">Login</button>
+               </div> 
             </div>
         )
     }
