00c31acdf67983df48caf0f8812e7672da3ff48e
lynellsinger <lynell.r.singer@gmail.com>
2020-11-26 15:40:29 -0800

    Created js file and updated sidebar layout

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+import PortfolioSidebarList from "../Portfolio/portfolio-sidebar-list";
+
 export default class PortfolioManager extends Component {
     constructor() {
         super()
@@ -34,7 +36,7 @@ export default class PortfolioManager extends Component {
                 </div>
 
                 <div className="right-column">
-                    <h1>Portfolio Sidebar...</h1>
+                    <PortfolioSidebarList data={this.state.portfolioItems}/>
                 </div>
             </div>
         )
