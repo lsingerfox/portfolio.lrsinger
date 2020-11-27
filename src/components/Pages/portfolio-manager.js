f36f1f3b2c707eaf0c89ef8410cca17cdeda52e4
lynellsinger <lynell.r.singer@gmail.com>
2020-11-26 16:39:42 -0800

    Started portfolio form page layout

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 
 import PortfolioSidebarList from "../Portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../Portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
     constructor() {
@@ -10,6 +11,19 @@ export default class PortfolioManager extends Component {
         this.state= {
             portfolioItems: []
         }
+
+        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+    }
+
+    handleSuccessfulFormSubmission(portfolioItem) {
+        // Todo
+        // Update portfolioItems state
+        // and add the portfolioItem to the list
+    }
+
+    handleFormSubmissionError(error) {
+        console.log("handleFormSubmissionError error", error)
     }
     
     getPortfolioItems() {
@@ -32,7 +46,10 @@ export default class PortfolioManager extends Component {
         return (
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
-                    <h1>Portfolio Form...</h1>
+                    <PortfolioForm 
+                        handleSuccessfulFormSubmission = {this.handleSuccessfulFormSubmission}
+                        handleFormSubmissionError = {this.handleFormSubmissionError}
+                    />
                 </div>
 
                 <div className="right-column">
---
+++ src/components/Portfolio/portfolio-form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class PortfolioForm extends Component {
+    render() {
+        return (
+            <div>
+                <h1>PortfolioForm</h1>
+            </div>
+        );
+    }
+}
