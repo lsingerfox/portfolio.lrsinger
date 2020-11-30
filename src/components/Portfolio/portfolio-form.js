7cdb1ffd67bf5640e1309f4a2caaea7f533fa7c3
lynellsinger <lynell.r.singer@gmail.com>
2020-11-29 22:01:30 -0800

    Merged and updated all js files

--- src/components/Portfolio/portfolio-form.js
+++ src/components/Portfolio/portfolio-form.js
@@ -19,14 +19,26 @@ export default class PortfolioForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    buildForm() {
+        let formData = new FormData();
+        
+        formData.append("portfolio_item[name]", this.state.name);
+        formData.append("portfolio_item[description]", this.state.description);
+        formData.append("portfolio_item[url]", this.state.url);
+        formData.append("portfolio_item[category]", this.state.category);
+        formData.append("portfolio_item[position]", this.state.position);
+        
+        return formData;
+    }
+
     handleChange(event) {
-        this.setState({
+        this.state ({
             [event.target.name]: event.target.value
         });
     }
 
     handleSubmit(event) {
-        console.log("event", event);
+        this.buildForm();
         event.preventDefault();
     }
 
--- src/components/app.js
+++ src/components/app.js
@@ -81,7 +81,7 @@ export default class App extends Component {
 
   authorizedRoutes () {
     return [
-      <Route path="/portfolio-manager" component ={PortfolioManager} />
+      <Route key="portfolio-manager" path="/portfolio-manager" component ={PortfolioManager} />
     ];
   }
   
