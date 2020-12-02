f3a866c68b8679a990a37976041cd7eaa03bbe54
lynellsinger <lynell.r.singer@gmail.com>
2020-12-02 12:19:12 -0800

    Updated design of sign out feature

--- src/components/Navigation/navigation-container.js
+++ src/components/Navigation/navigation-container.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import axios from "axios";
 import { withRouter } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavigationComponent = props => {
     const dynamicLink = (route, linkText) => {
@@ -63,7 +64,9 @@ const NavigationComponent = props => {
                 <div className="right-side">
                     LYNELL SINGER
 
-                    {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>Sign Out</a> : null }
+                    {props.loggedInStatus === "LOGGED_IN" ? <a onClick={handleSignOut}>
+                        <FontAwesomeIcon icon="sign-out-alt" />
+                    </a> : null }
                 </div>
             </div>
         )
