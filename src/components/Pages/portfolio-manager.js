9fb83cd23c576729c47b3804853909ef959b8854
lynellsinger <lynell.r.singer@gmail.com>
2020-12-01 23:03:32 -0800

    Finished delete process for portfolio items

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -18,7 +18,19 @@ export default class PortfolioManager extends Component {
     }
 
     handleDeleteClick(portfolioItem) {
-        console.log("handleDeleteClick", portfolioItem)
+        axios.delete(`https://lynellsinger.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`, { 
+            withCredentials: true 
+        }).then(response => {
+            this.setState({
+                portfolioItems: this.state.portfolioItems.filter(item => {
+                    return item.id !== portfolioItem.id;
+                })
+            });
+            
+            return response.data
+        }).catch(error => {
+            console.log("handleDeleteClick error", error);
+        })
     }
 
     handleSuccessfulFormSubmission(portfolioItem) {
