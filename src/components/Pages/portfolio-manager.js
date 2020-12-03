0b5d589d02e2396b69ae87c88c3d67c4461b240d
lynellsinger <lynell.r.singer@gmail.com>
2020-12-02 22:57:58 -0800

    Created code to edit and not add data in form

--- src/components/Pages/portfolio-manager.js
+++ src/components/Pages/portfolio-manager.js
@@ -13,7 +13,8 @@ export default class PortfolioManager extends Component {
             portfolioToEdit: {}
         }
 
-        this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
+        this.handleNewFormSubmission = this.handleNewFormSubmission.bind(this);
+        this.handleEditFormSubmission = this.handleEditFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
         this.handleEditClick = this.handleEditClick.bind(this);
@@ -48,7 +49,11 @@ export default class PortfolioManager extends Component {
         })
     }
 
-    handleSuccessfulFormSubmission(portfolioItem) {
+    handleEditFormSubmission() {
+        this.getPortfolioItems();
+    }
+
+    handleNewFormSubmission(portfolioItem) {
         this.setState({
             portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
         });
@@ -79,7 +84,8 @@ export default class PortfolioManager extends Component {
             <div className="portfolio-manager-wrapper">
                 <div className="left-column">
                     <PortfolioForm 
-                        handleSuccessfulFormSubmission = {this.handleSuccessfulFormSubmission}
+                        handleNewFormSubmission = {this.handleNewFormSubmission}
+                        handleEditFormSubmission = {this.handleEditFormSubmission}
                         handleFormSubmissionError = {this.handleFormSubmissionError}
                         clearPortfolioToEdit = {this.clearPortfolioToEdit}
                         portfolioToEdit={this.state.portfolioToEdit}
