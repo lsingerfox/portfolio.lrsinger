2615da91981c28942f85eb2c8298b95492c20ea3
lynellsinger <lynell.r.singer@gmail.com>
2020-12-06 15:54:49 -0800

    Created BlogItem component

---
+++ src/components/Blog/blog-item.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const BlogItem = props => {
+    const {
+        id,
+        blog_status,
+        content,
+        title,
+        featured_image_url
+    } = props.blogItem;
+
+    return (
+        <div>
+            <h1>{title}</h1>
+            <div>
+                {content}
+            </div>
+        </div>
+    )
+}
+
+export default BlogItem;
--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import BlogItem from "../Blog/blog-item";
+
 class Blog extends Component {
     constructor () {
         super();
@@ -30,7 +32,7 @@ class Blog extends Component {
 
     render () {
         const blogRecords = this.state.blogItems.map(blogItem => {
-            return <h1>{blogItem.title}</h1>;
+            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
         })
         return <div>{blogRecords}</div>
     }
