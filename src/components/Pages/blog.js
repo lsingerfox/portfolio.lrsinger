cfcc5b5ad9afaa6c40d1948ce0fcf9bc78b5f2ab
lynellsinger <lynell.r.singer@gmail.com>
2020-12-06 11:31:31 -0800

    Connected Blog API with axios get

--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Blog extends Component {
     constructor () {
         super();
+
+        this.state = {
+            blogItems: []
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this)
+    }
+
+    getBlogItems() {
+        axios.get("https://lynellsinger.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true} 
+        ).then (response => {
+            this.setState({
+                blogItems: response.data.portfolio_blogs
+            })
+        }).catch (error => {
+            console.log("getBlogItems error", error);
+        })
+    }
+
+    componentDidMount() {
+        this.getBlogItems();
     }
 
     render () {
