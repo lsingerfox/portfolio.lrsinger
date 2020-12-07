08e52e6f787c8da2e4a1e05d5dd308f2f5c5edf5
lynellsinger <lynell.r.singer@gmail.com>
2020-12-06 23:18:51 -0800

    Made ID call in web path dynamic

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogDetail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentBlogId: this.props.match.params.slug,
+            blogItem: {}
+        };
+    }
+
+    getBlogItem() {
+        axios.get(`https://lynellsinger.devcamp.space/portfolio/portfolio_blogs/${this.state.currentBlogId}`)
+        .then(response => {
+            console.log("response", response)
+        }).catch(error => {
+            console.log("getBlogItem error", error)
+        });
+    }
+
+    componentDidMount() {
+        this.getBlogItem();
+    }
+
     render() {
+        console.log("currentBlogId", this.state.currentBlogId)
         return (
             <div>
                 <h1>Blog Detail</h1>
