01c63a9d398f25f27281a7cc2abc595eb97a03d3
lynellsinger <lynell.r.singer@gmail.com>
2020-12-09 22:36:31 -0800

    Fixed any memory leaks with event listeners

--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -17,26 +17,30 @@ class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this)
-        this.activateInfiniteScroll();    
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);  
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
+    onScroll() {
+        if (this.state.isLoading || this.state.blogItems.length === this.state.totalCount) {
+            return;
+        }
 
-            if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
-                console.log("get more posts")
-            }
-        };
+        if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            this.getBlogItems();
+        }
     }
 
     getBlogItems() {
         this.setState ({
             currentPage: this.state.currentPage + 1
         })
-        axios.get("https://lynellsinger.devcamp.space/portfolio/portfolio_blogs", {withCredentials: true} 
+        axios.get(`https://lynellsinger.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {withCredentials: true} 
         ).then (response => {
+            console.log("getting", response.data);
             this.setState({
-                blogItems: response.data.portfolio_blogs,
+                blogItems: this.state.blogItems.concat
+                    (response.data.portfolio_blogs),
                 totalCount: response.data.meta.total_records,
                 isLoading: false
             })
@@ -47,6 +51,10 @@ class Blog extends Component {
 
     componentDidMount() {
         this.getBlogItems();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false)
     }
 
     render () {
