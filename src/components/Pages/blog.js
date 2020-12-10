701a53d22449605c1de5acca2a1d6d1121b93994
lynellsinger <lynell.r.singer@gmail.com>
2020-12-10 10:14:57 -0800

    Added Modal to Blog page

--- src/components/Pages/blog.js
+++ src/components/Pages/blog.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from 'axios';
 
 import BlogItem from "../Blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
     constructor () {
@@ -63,7 +64,7 @@ class Blog extends Component {
         })
         return (
             <div className="blog-container">
-
+                <BlogModal />
                 <div className="content-container">
                     {blogRecords}
                 </div>
