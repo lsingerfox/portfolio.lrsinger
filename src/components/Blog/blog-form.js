5e4e6708abbd2ffc8882ed10dc22b7915cfc662a
lynellsinger <lynell.r.singer@gmail.com>
2020-12-15 12:18:22 -0800

    Added blog form styles

--- src/components/Blog/blog-form.js
+++ src/components/Blog/blog-form.js
@@ -51,24 +51,27 @@ export default class BlogForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <input 
-                type="text"
-                onChange={this.handleChange} 
-                name="title"
-                placeholder="Blog Title"
-                value={this.state.title}
-                 />
+            <form onSubmit={this.handleSubmit} className="blog-form-wrapper">
+                <div className="two-column">
+                    <input 
+                    type="text"
+                    onChange={this.handleChange} 
+                    name="title"
+                    placeholder="Blog Title"
+                    value={this.state.title}
+                    />
 
-                <input 
-                type="text"
-                onChange={this.handleChange} 
-                name="blog_status"
-                placeholder="Blog Status"
-                value={this.state.blog_status}
-                 />
+                    <input 
+                    type="text"
+                    onChange={this.handleChange} 
+                    name="blog_status"
+                    placeholder="Blog Status"
+                    value={this.state.blog_status}
+                    />
 
-                <button>Save</button>
+                </div>
+
+                <button className="btn">Save</button>
             </form>
         )
     }
