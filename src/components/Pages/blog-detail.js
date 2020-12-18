2083631db70d15073ffd1ae6cfac63fe825da49a
lynellsinger <lynell.r.singer@gmail.com>
2020-12-17 19:00:01 -0800

    Added striptag & truncate libraries & updated blog

--- src/components/Pages/blog-detail.js
+++ src/components/Pages/blog-detail.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ReactHtmlParser from "react-html-parser";
+
+import BlogFeaturedImage from '../Blog/blog-featured-image';
 
 export default class BlogDetail extends Component {
     constructor(props) {
@@ -39,12 +42,10 @@ export default class BlogDetail extends Component {
                 <div className="content-container">
                     <h1>{title}</h1>
 
-                    <div className="featured-image-wrapper">
-                        <img src={featured_image_url} />
-                    </div>
+                    <BlogFeaturedImage img={featured_image_url} />
 
                     <div className="content">
-                        {content}
+                        { ReactHtmlParser(content) }
                     </div>
                 </div>
             </div>
