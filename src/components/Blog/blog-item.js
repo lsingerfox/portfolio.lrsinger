0b0a5fa0858d33ae10818353beb968a052bd91ee
lynellsinger <lynell.r.singer@gmail.com>
2020-12-17 20:27:35 -0800

    Created summary and removed tags in text

--- src/components/Blog/blog-item.js
+++ src/components/Blog/blog-item.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import striptags from 'striptags';
+import Truncate from 'react-truncate';
 
 const BlogItem = props => {
     const {
@@ -16,7 +18,16 @@ const BlogItem = props => {
                 <h1>{title}</h1>
             </Link>
             <div>
-                {content}
+                <Truncate 
+                    lines={5} 
+                    ellipsis={
+                        <span>
+                            ...<Link to={`/b/${id}`}>Read more</Link>
+                        </span>
+                    }
+                >
+                    { striptags(content) }
+                </Truncate>
             </div>
         </div>
     )
