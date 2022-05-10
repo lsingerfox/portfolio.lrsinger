97930488d4e8ceacfe013ccc1affa054ba22346d
lsingerfox <lynell.r.singer@gmail.com>
2022-05-09 19:38:33 -0700

    Added new about headshot photo, added new resume information to about.js, created about.scss and added to main.scss.

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import profilePicture from '../../../static/assets/images/bio/headshot.jpg';
+import profilePicture from '../../../static/assets/images/bio/headshot.png';
 
 export default function() {
     return (
@@ -14,7 +14,60 @@ export default function() {
                 }}
             />
             <div className="right-column">
-                Welcome! My name is Lynell Singer.
+                <div className='title'>
+                    Profile
+                </div>
+                <div className='profile'>
+                    A driven Front-End Web Developer interested in applying critical thinking and problem-solving skills to a work 
+                    environment that challenges growth in coding languages and creative skills. The experience presented shows an 
+                    ability to manage time between multiple tasks and multiple opportunities to raise diversity and disability 
+                    awareness. Additionally, the education provided shows a diverse array of subjects learned.
+                </div>
+                <div className='title'>
+                    Relevant Skills
+                </div>
+                <div className='one-column'>
+                    <ul>
+                        <li>Soft Skills:</li>
+                        <div className='two-colums'>
+                            <li>Critical thinking</li>
+                            <li>Willingness to learn</li>
+                            <li>Self-Leadership</li>
+                            <li>Self-Assessment</li>
+                            <li>Friendliness</li>
+                            <li>Empathy</li>
+                            <li>Problem-Solving</li>
+                            <li>Time Management</li>
+                            <li>Artistic Sense</li>
+                            <li>Business Etiquette</li>
+                            <li>Diversity Awareness</li>
+                            <li>Disability Awareness</li>
+                            <li>Customer Service</li>
+                            <li>Ability to work in a team</li>
+                        </div>
+                    </ul>
+                </div>
+                <div className='one-column'>
+                    <ul>
+                        <li>Technical Skills:</li>
+                        <div className='two-columns'>
+                            <li>JavaScript</li>
+                            <li>ReactJS</li>
+                            <li>Python 3</li>
+                            <li>UML</li>
+                            <li>HTML5</li>
+                            <li>CSS</li>
+                            <li>Flask</li>
+                            <li>JSON</li>
+                            <li>Git</li>
+                            <li>MongoDB</li>
+                            <li>SQL Databases</li>
+                            <li>SCSS/SASS</li>
+                            <li>Flexbox</li>
+                            <li>CSS Grid</li>
+                        </div> 
+                    </ul>
+                </div>
             </div>
         </div>
     )
